8786d7c6d9d906b42e34e1a5023bec99c1112677
FireIsGood <fireisadequate@gmail.com>
2025-08-02 05:07:01 -0700

    feat: keyboard shortcut base testing

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,8 +1,10 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { Post, PostComponent } from '../post/post.component';
 import { ActivatedRoute, Data } from '@angular/router';
 import { getFeed } from '../../scripts/post-store';
 import { LoaderComponent } from '../loader/loader.component';
+import { CallbackDictionary, GlobalKeydownService } from '../../services/global-keydown-service.service';
+import { Subject, takeUntil } from 'rxjs';
 
 export type Feed = 'explore' | 'cats';
 
@@ -12,9 +14,19 @@ export type Feed = 'explore' | 'cats';
   templateUrl: './feed.component.html',
 })
 export class FeedComponent {
+  navOffset = 80 + 16;
+
+  private KeyboardService = inject(GlobalKeydownService);
+  destroyEvent = new Subject();
+  shortcutList: CallbackDictionary = {
+    j: () => this.scrollToPost(1),
+    k: () => this.scrollToPost(-1),
+  };
+
   readonly routeData = signal<Data>({});
   readonly postList = signal<Post[]>([]);
   readonly feedName = computed<string>(() => this.routeData()['feed'] ?? 'Unknown');
+  postIndex: number = 0;
   loading = true;
 
   constructor(route: ActivatedRoute) {
@@ -23,6 +35,9 @@ export class FeedComponent {
 
   ngOnInit(): void {
     this.getFeed();
+    this.KeyboardService.keydownEvents.pipe(takeUntil(this.destroyEvent)).subscribe((key) => {
+      this.KeyboardService.handleKeydown(key, this.shortcutList);
+    });
   }
 
   async getFeed() {
@@ -32,5 +47,13 @@ export class FeedComponent {
       this.postList.set(feed);
     }
     this.loading = false;
+  }
+
+  scrollToPost(index: number) {
+    console.log(index);
+    this.postIndex = Math.max(Math.min(this.postIndex + index, this.postList().length - 1), 0);
+    const post = document.querySelector(`#post-${this.postIndex}`) as HTMLElement;
+    window.scrollTo(0, post.offsetTop - this.navOffset);
+    post.focus();
   }
 }
---
+++ src/app/services/global-keydown-service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable, filter, fromEvent, map } from 'rxjs';
+
+export type CallbackDictionary = Record<string, Function>;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GlobalKeydownService {
+  keydownEvents: Observable<string>;
+
+  constructor() {
+    this.keydownEvents = fromEvent<KeyboardEvent>(window, 'keydown').pipe(
+      filter((event) => !(event.target instanceof HTMLInputElement)),
+      map((event) => event.key)
+    );
+  }
+
+  handleKeydown(key: string, callbackDictionary: CallbackDictionary): void {
+    const callback = callbackDictionary[key];
+    if (callback === undefined) return;
+
+    callback();
+  }
+}
